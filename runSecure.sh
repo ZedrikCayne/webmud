412a71046aa30f5ab6e4e7cd087dd49b5b71b1fa
Zedrik Cayne <zedrikcayne@gmail.com>
2025-07-07 12:55:22 -0400

    Okay, fixed websockets off by a few bytes when pushing hard on it.

--- runSecure.sh
+++ runSecure.sh
@@ -1,3 +1,3 @@
-gdb --args build/webmud --port 8443 --key secrets/key.pem --certificate secrets/certificate.pem --log-access
-#build/webmud --port 8443 --key secrets/key.pem --certificate secrets/certificate.pem --log-access
+#gdb --args build/webmud --port 8443 --key secrets/key.pem --certificate secrets/certificate.pem --log-access
+build/webmud --port 8443 --key secrets/key.pem --certificate secrets/certificate.pem --log-access
 
